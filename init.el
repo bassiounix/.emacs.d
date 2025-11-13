3ded03a27e1104d49f6b2d80c1a1431305242b6d
bassiounix <muhammad.m.bassiouni@gmail.com>
2025-11-14 00:00:24 +0200

    update

--- init.el
+++ init.el
@@ -1,6 +1,13 @@
 (setq display-line-numbers-type 'relative)
 
-(load-theme 'tango-dark t)
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+
+(load-theme 'github-dark t)
+
+(set-face-attribute 'default nil
+  :font "Dank Mono" ;; font name
+  :height 120)      ;; font size (1/10 pt units: 120 = 12pt)
+
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
