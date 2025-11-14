a55958d6fe541b5d48335b6b27a27f2c220e9ffb
bassiounix <muhammad.m.bassiouni@gmail.com>
2025-11-14 03:27:22 +0200

    update

--- init.el
+++ init.el
@@ -3,14 +3,20 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 
 (load-theme 'github-dark t)
-
-(set-face-attribute 'default nil
-  :font "Dank Mono" ;; font name
-  :height 120)      ;; font size (1/10 pt units: 120 = 12pt)
-
+(add-to-list 'default-frame-alist '(font . "Dank Mono-14"))
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
 (global-display-line-numbers-mode)
 (show-paren-mode 1)
 (ido-mode)
+
+;; optional if you want which-key integration
+(use-package which-key
+    :config
+    (which-key-mode))
+
+(use-package autothemer)
+
+;;(add-hook 'prog-mode-hook #'lsp-deferred)
+;;(use-package lsp-java :config (add-hook 'java-mode-hook 'lsp-deferred))
